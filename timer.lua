40322e779b07b05c1f9cca6316c140a2a91efddc
Demit <83365420+SasnyParen@users.noreply.github.com>
2023-06-06 21:36:04 +0300

    Update timer.lua
    Add timer.Pause(), timer.UnPause(), timer.Stop(), timer.Start(), timer.Toggle(), timer.RepsLeft()

--- timer.lua
+++ timer.lua
@@ -3,10 +3,13 @@ timer = {}
 local timer_data = {}
 local timer_list = {}
 
-function timer.Create(name, time, func)
+function timer.Create(name, time, replys, func)
 	timer_data[name] = { 
 		time = love.timer.getTime() + time,
+		true_time = time,
 		func = func,
+		reply = replys,
+		stop = false,
 	}
 	timer_list[name] = {}
 	return true
@@ -16,6 +19,9 @@ function timer.Simple(time, func)
 	timer_data[#timer_data + 1] = { 
 		time = love.timer.getTime() + time,
 		func = func,
+		reply = 1,
+		true_time = time,
+		stop = false,
 	}
 	timer_list[#timer_data] = {}
 	return true
@@ -32,10 +38,11 @@ function timer.DestroyAll()
 end
 
 function timer.TimeLeft(name)
-	if timer_data[name] then 
-		return math.floor(timer_data[name].time - love.timer.getTime())
-	end
-	return 0
+	if not timer_list[name] then return 0 end
+
+	if timer_data[name].stop then return timer_data[name].stop_time or timer_data[name].time end
+	return timer_data[name].time - love.timer.getTime()
+
 end
 
 function timer.Exists(name)
@@ -46,13 +53,60 @@ function timer.Exists(name)
 	end
 end
 
-function UpdateTimers()
+function timer.Pause(name)
+	if not timer_list[name] then return false end 
+	local new_time = (timer_data[name].time - love.timer.getTime())
+	timer_data[name].stop = true
+	timer_data[name].stop_time = new_time
+	timer_data[name].true_time = new_time
+	return true
+end
+
+function timer.UnPause(name)
+	if not timer_list[name] then return false end 
+	timer_data[name].stop = false
+	timer_data[name].time = love.timer.getTime() + timer_data[name].true_time
+	return true
+end
+
+function timer.Stop(name)
+	if not timer_list[name] then return false end 
+	timer_data[name].stop = true
+	return true
+end
+
+function timer.Start(name)
+	if not timer_list[name] then return false end 
+	timer_data[name].stop = false
+	timer_data[name].time = love.timer.getTime() + timer_data[name].true_time
+	return true
+end
+
+function timer.Toggle(name)
+	if not timer_list[name] then return false end 
+	if timer_data[name].stop then timer.UnPause(name) else timer.Pause(name) end
+end
+
+function timer.RepsLeft(name)
+	if not timer_list[name] then return false end 
+	return timer_data[name].reply
+end
+
+
+function timer.Update()
 
 	for i, timer in pairs(timer_data) do 
-		if love.timer.getTime() >= timer.time then 
-			timer_list[i] = nil
-			timer_data[i] = nil
-			timer.func()
+		if love.timer.getTime() >= timer.time and not timer.stop then 
+			if timer.reply == 0 then 
+				timer_data[i].time = love.timer.getTime() + timer.true_time
+				timer.func()
+			elseif timer.reply >= 1 then
+				local new_reply = timer.reply - 1
+				timer_data[i].time = love.timer.getTime() + timer.true_time
+				timer_data[i].reply = new_reply
+				if new_reply == 0 then timer_list[i] = nil timer_data[i] = nil end
+				timer.func()
+			end
 		end
 	end
 
